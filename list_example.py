7a0bf68f65bcd27a4693dd1d6aeb4d784b19e489
ahmadzhafif24 <88156184+ahmadzhafif24@users.noreply.github.com>
2025-12-30 14:12:34 +0700

    list_example.py

---
+++ list_example.py
@@ -0,0 +1,18 @@
+# list_example.py
+
+# Membuat list
+buah = ["apel", "pisang", "mangga"]
+
+# Menampilkan list
+print("Daftar buah:", buah)
+
+# Menambah data
+buah.append("jeruk")
+print("Setelah ditambah:", buah)
+
+# Mengakses elemen list
+print("Buah pertama:", buah[0])
+
+# Menghapus data
+buah.remove("pisang")
+print("Setelah dihapus:", buah)
