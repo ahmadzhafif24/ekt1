940ae1fec96901e7390b0ccc92a7f187fa0d2ed4
ahmadzhafif24 <88156184+ahmadzhafif24@users.noreply.github.com>
2025-12-30 14:15:05 +0700

    Create looping_python.py

---
+++ looping_python.py
@@ -0,0 +1,19 @@
+# looping_python.py
+
+# Looping menggunakan for
+print("Looping for:")
+for i in range(1, 6):
+    print("Angka:", i)
+
+# Looping list
+buah = ["apel", "pisang", "mangga"]
+print("\nLooping list:")
+for b in buah:
+    print(b)
+
+# Looping menggunakan while
+print("\nLooping while:")
+x = 1
+while x <= 3:
+    print("Nilai x:", x)
+    x += 1
