ea3eeba03b1530ad602607960f786a05ce6b856a
ahmadzhafif24 <88156184+ahmadzhafif24@users.noreply.github.com>
2025-12-30 14:14:19 +0700

    Create set_example.py

---
+++ set_example.py
@@ -0,0 +1,14 @@
+# set_example.py
+
+# Membuat set
+angka = {1, 2, 3, 3, 4}
+
+print("Isi set:", angka)  # duplikat otomatis hilang
+
+# Menambah data
+angka.add(5)
+print("Setelah ditambah:", angka)
+
+# Menghapus data
+angka.remove(2)
+print("Setelah dihapus:", angka)
