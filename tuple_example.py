4c8635b7b364038f7b84d7ce2bb2ec039f079f69
ahmadzhafif24 <88156184+ahmadzhafif24@users.noreply.github.com>
2025-12-30 14:13:27 +0700

    Add tuple_example.py

---
+++ tuple_example.py
@@ -0,0 +1,13 @@
+# tuple_example.py
+
+# Membuat tuple
+koordinat = (10, 20)
+
+print("Koordinat:", koordinat)
+
+# Mengakses data tuple
+print("X =", koordinat[0])
+print("Y =", koordinat[1])
+
+# Tuple tidak bisa diubah
+# koordinat[0] = 5  # ini akan error
